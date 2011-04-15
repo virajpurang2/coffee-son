577ab601e9566616fe2f1a170970e4218478b590
Ben Atkin <ben@benatkin.com>
2011-04-14 20:25:13 -0600

    add object example

---
+++ examples/object.coffee
@@ -0,0 +1,12 @@
+# TODO replace with hex
+colors =
+  red:
+    fuchsia: 1
+    crimson: 1
+  green:
+    forest: 1
+  blue:
+    turquoise: 1
+
+# TODO print in a JSON file
+console.log JSON.stringify(colors)
---
+++ examples/object.js
@@ -0,0 +1,16 @@
+(function() {
+  var colors;
+  colors = {
+    red: {
+      fuchsia: 1,
+      crimson: 1
+    },
+    green: {
+      forest: 1
+    },
+    blue: {
+      turquoise: 1
+    }
+  };
+  console.log(JSON.stringify(colors));
+}).call(this);
